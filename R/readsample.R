8ec063f0b3ab4723a43b540108bd967b6891ea7c
Peter Cowan <pdc@pcowan.com>
2008-02-26 07:21:21 +0000

    R CMD check prefers T & F be TRUE and FALSE

--- R/readsample.R
+++ R/readsample.R
@@ -1,6 +1,6 @@
 `readsample` <-
 function (filename = "") {
-    x <- read.table(file = filename, header = F, sep = "\t", col.names = c("plot", 
+    x <- read.table(file = filename, header = FALSE, sep = "\t", col.names = c("plot", 
         "abund", "id"))
     sample2matrix(x)
 }
--- R/writesample.R
+++ R/writesample.R
@@ -1,6 +1,6 @@
 `writesample` <-
 function (community, filename = "") {
     write.table(matrix2sample(community), file = filename, append = FALSE, 
-        sep = "\t", eol = "\n", quote = F, row.names = F, col.names = F)
+        sep = "\t", eol = "\n", quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
 
