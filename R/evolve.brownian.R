666ef0c1d4758d521bd2eac45884532ba2f2cd31
Steven Kembel <steve.kembel@gmail.com>
2008-04-26 00:03:39 +0000

    Further combining of functions into single aggregated files

--- R/evolve.brownian.R
+++
@@ -1,6 +0,0 @@
-`evolve.brownian` <-
-function(phy,value=0,var=1) {
-	x <- as.vector(t(evolve.phylo(phy,value,var)$tip.character))
-	names(x) <- phy$tip.label
-	return(x)
-}
--- R/sample.prune.R
+++
@@ -1,9 +0,0 @@
-`sample.prune` <-
-function (samp, phylo) 
-{
-    treeTaxa <- phylo$tip.label
-    sampleTaxa <- colnames(samp)
-    trimTaxa <- setdiff(treeTaxa, sampleTaxa)
-    if (length(trimTaxa) > 0) drop.tip(phylo, trimTaxa) else phylo
-}
-
