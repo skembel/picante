13801903cc90f6d1b06d25e8f39f52e1e74f101f
Steven Kembel <steve.kembel@gmail.com>
2008-06-01 06:04:13 +0000

    Final cleaning of documentation and tagging of code for submission to CRAN (version 0.1-2)

--- R/utility.R
+++ R/utility.R
@@ -69,12 +69,6 @@ function(phy) {
 	return(phy)
 	}
 
-`.phylo2phylog` <-
-function(phy, ...) {
-    if(!require(ade4)) {stop("This function requires the ade4 package")}
-    newick2phylog(write.tree(phy),...)
-}
-
 `sortColumns` <-
 function(x) {
 
