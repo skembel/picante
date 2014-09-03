16e86a86914b923287a646e3b6a99e645ddb59a3
Jari Oksanen <jari.oksanen@oulu.fi>
2014-09-03 16:40:58 +0300

    fix issue #1 matrix2sample fails if rownames missing

--- R/phylocom.R
+++ R/phylocom.R
@@ -15,7 +15,8 @@ function(x) {
 
 `matrix2sample` <-
 function(z) {
-	temp <- data.frame(expand.grid(dimnames(z))[1:2], as.vector(as.matrix(z)))
+	temp <- data.frame(expand.grid(dimnames(provideDimnames(z)))[1:2],
+                           as.vector(as.matrix(z)))
 	temp <- temp[(temp[, 3] > 0) & !is.na(temp[, 3]), ]
 	temp <- temp[sort.list(temp[, 1]), ]
 	data.frame(plot=temp[, 1], abund=temp[, 3], id=temp[, 2])
